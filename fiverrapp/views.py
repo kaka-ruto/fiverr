fe789c4d2ad35c4ba21f72067ac6ea1f454efa6b
borenho <borecollins@gmail.com>
2016-12-13 01:05:21 +0300

    Connected the models to the views and updated the templates

--- fiverrapp/views.py
+++ fiverrapp/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Gig
 
 def home(request):
-    return render(request, 'home.html', {})
+    gigs = Gig.objects.filter(status=True)
+    return render(request, 'home.html', {"gigs": gigs})
 
 
 def gig_detail(request, id):
-    return render(request, 'gig_detail.html', {})
+    try:
+        gig = Gig.objects.get(id=id)
+    except Gig.DoesNotExist:
+        return redirect('/')
+    return render(request, 'gig_detail.html', {"gig": gig})
